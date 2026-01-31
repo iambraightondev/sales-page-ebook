8a6f2138ec15940ee21f574e36315face476f472
iambraightondev <braightonfoulman@gmail.com>
2026-01-30 19:37:57 -0500

    fix checkout link

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
   }, []);
 
   const handleCTAClick = () => {
-    window.open('https://pay.hotmart.com/M104165239G', '_blank');
+    window.open('https://pay.hotmart.com/M104165239G?checkoutMode=10', '_blank');
   };
 
   return (
